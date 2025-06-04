7a22600ce96ac060592db5c52085717456de61a2
Mohammad Irfan <irfan.noorzada123@gmail.com>
2025-06-04 13:46:27 +0430

    Initial commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function Main() {
   return (
-    <main className="main text-center px-3 py-3 md:text-left xl:px-32 xl:py-[2.9rem] lg:px-20 lg:py-16 md:px-12 md:p-10">
+    <main className="main text-center px-3 py-3 md:text-left xl:pr-10 xl:pl-32 xl:py-[2.9rem] lg:px-20 lg:py-16 md:px-12 md:p-10">
       <div className="wrapper flex justify-around flex-col md:gap-10 md:flex-row ">
 
         <div className="content mt-16 md:mt-0 gap-10 sm:mt-24 sm:mx-auto md:mx-0 md:gap-12 xl:pt-[6.5rem] lg:pt-20 md:pt-16 flex flex-col justify-between
-          sm:w-[38rem] md:w-[32.8rem] order-1 md:order-none">
+          sm:w-[38rem] md:w-[33.2rem] order-1 md:order-none">
           <div className="">
             <h1 className="text-4xl md:text-[clamp(3rem,6vw,5rem)] font-bold leading-[1]">Make remote work</h1>
             <p className="text[1rem] md:text-[clamp(1.125rem,6vw,1rem)] text-mediumGray font-medium leading-7 xl:pr-20 md:pr-10 xl:mt-[3rem]
@@ -16,7 +16,7 @@ function Main() {
             <button className="py-2.5 px-5 xl:pt-3.5 xl:pb-3 xl:px-8 bg-almostBlack text-white font-bold font-epilogue text-[1rem] lg:text-base md:rounded-2xl
               rounded-[0.9rem] border-2 border-almostBlack duration-300 hover:bg-transparent hover:text-almostBlack">Learn more</button>
           </div>
-          <div className="flex flex-wrap md:flex-nowrap items-center justify-center md:justify-start gap-7 xl:gap-7 lg:gap-5 md:gap-4 xl:-mb-2">
+          <div className="flex flex-wrap md:flex-nowrap items-center justify-center md:justify-start gap-7 xl:gap-7 lg:gap-5 md:gap-4">
             <img src="images/client-databiz.svg" alt="databiz" className="w-20 lg:w-24 xl:w-auto" />
             <img src="images/client-audiophile.svg" alt="audiophile" className="w-12 md:w-14 lg:w-24 xl:w-auto" />
             <img src="images/client-meet.svg" alt="meet" className="w-16 md:w-20 lg:w-24 xl:w-auto" />
